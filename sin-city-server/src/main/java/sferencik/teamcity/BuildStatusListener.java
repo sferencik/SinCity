a21678dd7efa423b815fd187993281bef792739f
Sam Ferencik <sferencik@gmail.com>
2015-07-16 22:32:28 +0200

    * clean up logging (fewer lines, more compact, more logical order) * add info about predecessor * refactor buildFinishedWithTeamCity into two methods

--- sin-city-server/src/main/java/sferencik/teamcity/BuildStatusListener.java
+++ sin-city-server/src/main/java/sferencik/teamcity/BuildStatusListener.java
@@ -37,71 +37,57 @@ public class BuildStatusListener
             @Override
             public void buildFinished(@NotNull SRunningBuild build)
             {
-                final List<SVcsModification> containingChanges = build.getContainingChanges();
 
-                Loggers.SERVER.debug("[SinCity] build id: " + build.getBuildId());
-                Loggers.SERVER.debug("[SinCity] build description: " + build.getBuildDescription());
-                Loggers.SERVER.debug("[SinCity] build successful: " + build.getBuildStatus().isSuccessful());
-                Loggers.SERVER.debug("[SinCity] number of build changes: " + containingChanges.size());
-                Loggers.SERVER.debug("[SinCity] build changes: " + containingChanges);
+                Loggers.SERVER.debug("[SinCity] build: " + build);
 
                 final SBuildType buildType = build.getBuildType();
                 if (buildType == null)
                     return;
 
+                SFinishedBuild previousBuild = build.getPreviousFinished();
+                Loggers.SERVER.debug("[SinCity] previous build: " + previousBuild);
+
+                final List<SVcsModification> containingChanges = build.getContainingChanges();
+                Loggers.SERVER.debug("[SinCity] changes (" + containingChanges.size() + "): " + containingChanges);
+
                 for (SBuildFeatureDescriptor feature : buildType.getBuildFeatures()) {
                     final Class<? extends BuildFeature> featureClass = feature.getBuildFeature().getClass();
                     Loggers.SERVER.debug("[SinCity] found plugin: " + featureClass);
                     if (featureClass.equals(SinCityBuildFeature.class)) {
-                        buildFinishedWithSinCity(build, buildType, feature, containingChanges);
+                        tagBuild(build, feature);
+                        if (build.getBuildStatus().isSuccessful())
+                            return;
+                        if (containingChanges.size() <= 1)
+                            return;
+                        triggerCulpritFinding(buildType, build, previousBuild, containingChanges);
                         break; // for now we only allow a single SinCity feature (see SinCityBuildFeature.isMultipleFeaturesPerBuildTypeAllowed())
                     }
                 }
             }
 
-            private void buildFinishedWithSinCity(
-                    SRunningBuild build,
+            private void triggerCulpritFinding(
                     SBuildType buildType,
-                    SBuildFeatureDescriptor sinCityFeature,
+                    SRunningBuild thisBuild,
+                    SFinishedBuild previousBuild,
                     List<SVcsModification> containingChanges)
             {
-                // tag the finished build
-                final String triggeredBySinCityParameterValue = build.getParametersProvider().get(triggeredBySinCityParameterName);
-                String tagParameterName = triggeredBySinCityParameterValue == null
-                        ? "nonSinCityTag"
-                        : "sinCityTag";
-                final String tagName = sinCityFeature.getParameters().get(tagParameterName);
-                if (tagName != null && !tagName.isEmpty()) {
-                    Loggers.SERVER.debug("[SinCity] tagging build with '" + tagName + "'");
-                    final List<String> resultingTags = new ArrayList<String>(build.getTags());
-                    resultingTags.add(tagName);
-                    build.setTags(resultingTags);
-                }
-
-                // nothing to investigate unless we've failed
-                if (build.getBuildStatus().isSuccessful())
-                    return;
-
                 // identify the build problems
                 // TODO: we won't do anything about these as yet but later these could be used with a feature "only run SinCity if there are new failures"
                 /*
-                final List<BuildProblemData> failureReasons = build.getFailureReasons();
+                final List<BuildProblemData> failureReasons = thisBuild.getFailureReasons();
                 for (BuildProblemData failureReason : failureReasons) {
                     Loggers.SERVER.warn("Failure [" + failureReason.getIdentity() + "]; " + failureReason.getDescription() + "|" + failureReason);
                 }
-                Loggers.SERVER.warn("Have " + build.getTestMessages(0, -1).size() + " messages.");
-                for (TestInfo info : build.getTestMessages(0, -1)) {
+                Loggers.SERVER.warn("Have " + thisBuild.getTestMessages(0, -1).size() + " messages.");
+                for (TestInfo info : thisBuild.getTestMessages(0, -1)) {
                     Loggers.SERVER.warn("Test " + info.getName() + ", status " + info.getStatus());
                 }
                 */
 
-                if (containingChanges.size() <= 1)
-                    return;
-
                 Loggers.SERVER.info("[SinCity] will look for culprit");
-//                Loggers.SERVER.info(">>>>>>> Is historical: " + build.isOutdated());
+//                Loggers.SERVER.info(">>>>>>> Is historical: " + thisBuild.isOutdated());
 //                Loggers.SERVER.info(">>>>>>> # changes " + containingChanges.size());
-//                Loggers.SERVER.info(">>>>>>> Failed build after change " + containingChanges.get(0).getDescription());
+//                Loggers.SERVER.info(">>>>>>> Failed thisBuild after change " + containingChanges.get(0).getDescription());
 
                 // create a list of suspects
                 List<SVcsModification> suspectChanges = new ArrayList<SVcsModification>(containingChanges);
@@ -110,12 +96,27 @@ public class BuildStatusListener
 
                 BuildCustomizer buildCustomizer = buildCustomizerFactory.createBuildCustomizer(buildType, null);
                 for (SVcsModification change : suspectChanges) {
-                    Loggers.SERVER.info("[SinCity] Queueing change " + change.getDescription() + " having failed build " + build.getBuildNumber());
+                    Loggers.SERVER.info("[SinCity] Queueing change " + change.getDescription() + " having failed build " + thisBuild.getBuildNumber());
                     buildCustomizer.setChangesUpTo(change);
                     Map<String, String> parameters = new HashMap<String, String>();
-                    parameters.put("triggered.by.sin.city", build.getBuildNumber());
+                    parameters.put("triggered.by.sin.city", thisBuild.getBuildNumber());
                     buildCustomizer.setParameters(parameters);
-                    buildCustomizer.createPromotion().addToQueue("SinCity, rerun of " + build.getBuildDescription());
+                    buildCustomizer.createPromotion().addToQueue("SinCity, rerun of " + thisBuild.getBuildDescription());
+                }
+            }
+
+            private void tagBuild(SRunningBuild thisBuild, SBuildFeatureDescriptor sinCityFeature) {
+                // tag the finished build
+                final String triggeredBySinCityParameterValue = thisBuild.getParametersProvider().get(triggeredBySinCityParameterName);
+                String tagParameterName = triggeredBySinCityParameterValue == null
+                        ? "nonSinCityTag"
+                        : "sinCityTag";
+                final String tagName = sinCityFeature.getParameters().get(tagParameterName);
+                if (tagName != null && !tagName.isEmpty()) {
+                    Loggers.SERVER.debug("[SinCity] tagging build with '" + tagName + "'");
+                    final List<String> resultingTags = new ArrayList<String>(thisBuild.getTags());
+                    resultingTags.add(tagName);
+                    thisBuild.setTags(resultingTags);
                 }
             }
 
