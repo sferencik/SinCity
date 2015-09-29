cfc3d6f69b19d3a84840547cdbcb1ea41061a62c
Ferencik, Samuel: Markets (PRG) <Samuel.Ferencik@barclayscapital.com>
2015-09-29 15:14:42 +0000

    allow getSincityRangeTopBuildId() to be defined but empty - this still means we're not triggered by SinCity
    [git-p4: depot-paths = "//depot/QA/Toolbox/main.br/third_party_src/SinCity/": change = 10675456]

--- sin-city-server/src/main/java/sferencik/teamcity/sincity/BuildTagger.java
+++ sin-city-server/src/main/java/sferencik/teamcity/sincity/BuildTagger.java
@@ -24,7 +24,13 @@ public class BuildTagger {
     void tagBuild() {
         // tag the finished build
         SettingNames settingNames = new SettingNames();
-        String tagParameterName = build.getParametersProvider().get(new ParameterNames().getSincityRangeTopBuildId()) == null
+
+        // if this is a SinCity-triggered build, we know for sure that the getSincityRangeTopBuildId() parameter is set;
+        // if, on the other hand, it is a non-SinCity-triggered run, it may still be set (and hopefully empty) if the
+        // user has set the parameter up in their build configuration; therefore, don't just test for null and accept
+        // empty as a sign of a non-SinCity-triggered build
+        final String sincityRangeTopBuildId = build.getParametersProvider().get(new ParameterNames().getSincityRangeTopBuildId());
+        String tagParameterName = sincityRangeTopBuildId == null || sincityRangeTopBuildId.isEmpty()
                 ? settingNames.getTagNameForBuildsNotTriggeredBySinCity()
                 : settingNames.getTagNameForBuildsTriggeredBySinCity();
         final String tagName = sinCityParameters.get(tagParameterName);
