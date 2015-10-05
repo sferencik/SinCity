59253b3276964c1a35cafbdd273a4244f41d0cf4
Ferencik, Samuel: Markets (PRG) <Samuel.Ferencik@barclayscapital.com>
2015-10-05 16:09:49 +0000

    store the full name of each test (as best as we can)
    [git-p4: depot-paths = "//depot/QA/Toolbox/main.br/third_party_src/SinCity/": change = 10686293]

--- sin-city-server/src/main/java/sferencik/teamcity/sincity/json/TestName.java
+++ sin-city-server/src/main/java/sferencik/teamcity/sincity/json/TestName.java
@@ -3,7 +3,11 @@ package sferencik.teamcity.sincity.json;
 import com.google.gson.annotations.SerializedName;
 
 public class TestName {
+    private String fullName;
+
     private String suite;
+
+    private String fullNameWithoutSuite;
 
     @SerializedName("package")
     private String packageName;
@@ -16,7 +20,9 @@ public class TestName {
     private String parameters;
 
     public TestName(jetbrains.buildServer.tests.TestName tcTestName) {
+        fullName = Util.nullifyEmptyString(tcTestName.getAsString());
         suite = Util.nullifyEmptyString(tcTestName.getSuite());
+        fullNameWithoutSuite = Util.nullifyEmptyString(tcTestName.getNameWithoutSuite());
         packageName = Util.nullifyEmptyString(tcTestName.getPackageName());
         className = Util.nullifyEmptyString(tcTestName.getClassName());
         test = Util.nullifyEmptyString(tcTestName.getTestMethodName());
